c7d9bcd6dd840287c9caf021990a1266b32e0c7b
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-10-10 13:01:21 +0530

    added subcompoents

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import NewNavbar from "./components/NewNavbar";
+import Home from "./components/Home";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container>
+      <NewNavbar />
+      <Home />
+    </Container>
+  );
 }
 
 export default App;
