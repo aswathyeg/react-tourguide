e5f01ea41d2632f5f312fafcfd73c3cb4a0fc6d9
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-11-08 17:14:20 +0530

    added image slide object

---
+++ src/components/data.js
@@ -0,0 +1,18 @@
+const imageSlide = [
+  {
+    url: "photo1.jpg",
+    title: "photo1",
+    body: "photo1body",
+  },
+  {
+    url: "photo2.jpg",
+    title: "photo2",
+    body: "photo2body",
+  },
+  {
+    url: "photo3.jpg",
+    title: "photo3",
+    body: "photo3body",
+  },
+];
+export default imageSlide;
