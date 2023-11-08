afe84419e0a3959ca04f53c9c8aee9a81bba3780
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-11-08 18:09:42 +0530

    added state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
-
+import imageSlide from "./data";
 import "react-awesome-slider/dist/styles.css";
 import photo1 from "./assets/photo1.avif";
 const Home = () => {
-  return <div>Home</div>;
+  const [currentState, setCurrentState] = useState(0);
+  return <div className="container-style">Home</div>;
 };
 
 export default Home;
