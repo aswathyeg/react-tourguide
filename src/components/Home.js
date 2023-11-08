a1d9bdb07764417b55a117f054391407e42c401c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-11-08 17:13:35 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
+import "./Home.css";
 
+import "react-awesome-slider/dist/styles.css";
+import photo1 from "./assets/photo1.avif";
 const Home = () => {
   return <div>Home</div>;
 };
