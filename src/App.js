6c20ca64858ce1a02c6da70eaa136787cce24e9c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-11-10 22:48:57 +0530

    moved navbar to Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import NewNavbar from "./components/NewNavbar";
+
 import Home from "./components/Home";
 import Container from "react-bootstrap/Container";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -7,17 +7,11 @@ import Books from "./components/Books";
 import Location from "./components/Location";
 function App() {
   return (
-    <Container>
-      <NewNavbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/location" element={<Location />} />
-          <Route path="/books" element={<Books />} />
-        </Routes>
-      </BrowserRouter>
-      {/* <Home /> */}
-    </Container>
+    <div>
+      <Home />
+      {/* <Route path="/location" element={<Location />} />
+          <Route path="/books" element={<Books />} /> */}
+    </div>
   );
 }
 
