4bfb2fdc019663072d4b7a3a03b88b19299e7b0e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-11-10 22:49:36 +0530

    added navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,36 @@ import "./Home.css";
 import imageSlide from "./data";
 import "react-awesome-slider/dist/styles.css";
 import photo1 from "./assets/photo1.avif";
+import NewNavbar from "./NewNavbar";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Books from "./Books";
+import Location from "./Location";
+import { Card } from "react-bootstrap";
+
 const Home = () => {
   const [currentState, setCurrentState] = useState(0);
-  return <div className="container-style">Home</div>;
+  const bgImageStyle = {
+    backgroundImage: `url(${imageSlide[currentState].url})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    height: "100vh",
+    // width: "100 vw",
+  };
+  return (
+    <div className="container-style">
+      <div style={bgImageStyle}>
+        {" "}
+        <NewNavbar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/location" element={<Location />} />
+            <Route path="/books" element={<Books />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
