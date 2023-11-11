105430626382f643314686e042bbf74e7ca6eeae
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-11-11 21:33:56 +0530

    added destinations

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
 import Container from "react-bootstrap/Container";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Books from "./components/Books";
 import Location from "./components/Location";
+import Destinations from "./components/Destinations";
 function App() {
   return (
     <div>
       <Home />
       {/* <Route path="/location" element={<Location />} />
           <Route path="/books" element={<Books />} /> */}
+      <Destinations />
     </div>
   );
 }
