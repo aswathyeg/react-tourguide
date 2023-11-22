25cafb1a50194b934823168b743d61ed880f28f5
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-11-22 12:49:16 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Books from "./components/Books";
 import Location from "./components/Location";
 import Destinations from "./components/Destinations";
+import NewNavbar from "./components/NewNavbar";
 function App() {
   return (
     <div>
+      <NewNavbar />
       <Home />
       {/* <Route path="/location" element={<Location />} />
           <Route path="/books" element={<Books />} /> */}
