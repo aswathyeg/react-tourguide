6a402f48f3bde07b3bc69ce1bde9676e26492adc
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-11-23 20:28:07 +0530

    added props

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,11 +10,8 @@ const Card = (props) => {
       <div class="card p0 overflow-hidden h-100 shadow">
         <img src={props.img} class="card-img-top" alt="..." />
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <h5 class="card-title">{props.title}</h5>
+          <p class="card-text">{props.description}</p>
           {/* <a href="#" class="btn btn-primary">
             Go somewhere
           </a> */}
