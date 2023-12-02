6511634b327f92dbedaedec8c383b9b52a759877
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2023-12-02 20:50:30 +0530

    footer

---
+++ src/components/FooterStyles.js
@@ -0,0 +1,70 @@
+// components/FooterStyles.js
+
+
+import styled from "styled-components";
+
+export const Box = styled.div`
+	padding: 5% 2.5%;
+	background: white;
+	// position: absolute;
+	bottom: 0;
+	width: 95%;
+
+	@media (max-width: 1000px) {
+		// padding: 70px 30px;
+	}
+`;
+
+export const FooterContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	max-width: 1000px;
+	margin: 0 auto;
+	/* background: red; */
+`;
+
+export const Column = styled.div`
+	display: flex;
+	flex-direction: column;
+	// text-align: left;
+	margin-left: 60px;
+`;
+
+export const Row = styled.div`
+	display: grid;
+	grid-template-columns: repeat(
+		auto-fill,
+		minmax(185px, 1fr)
+	);
+	grid-gap: 20px;
+
+	@media (max-width: 1000px) {
+		grid-template-columns: repeat(
+			auto-fill,
+			minmax(200px, 1fr)
+		);
+	}
+`;
+
+export const FooterLink = styled.a`
+	//  color: rgb(104, 236, 153);
+	margin-bottom: 20px;
+	font-size: 18px;
+	text-decoration: none;
+	font-family: 'Kanit'
+
+	&:hover {
+		color: green;
+		transition: 200ms ease-in;
+		text-decoration: none;
+	}
+`;
+
+export const Heading = styled.p`
+	font-size: 24px;
+	color: green;
+	margin-bottom: 40px;
+	font-weight: bold;
+	font-family: 'Kanit'
+`;
